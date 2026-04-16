3ae77cffd54d6c7ace3775dc200b9144ecfce75e
walter-202 <wfernando.aguilarm@gmail.com>
2026-04-16 11:40:56 -0400

    Ejercicio 5 y arreglo de ejercicio 4 sin Safe Mode de SQL Workbench

--- Walter Aguilar Mayta/EjerciciosDatos/Datosunifest.sql
+++ Walter Aguilar Mayta/EjerciciosDatos/Datosunifest.sql
@@ -11,15 +11,13 @@ INSERT INTO usuarios (usuario, contrasena, correo, nombre_completo, id_rol) VALU
 ('pepito','pepito2025','pepito@unifest.edu','Carlos Rivera',5);
 -- Ejercicio 3: Cancelar una entrada
 UPDATE entradas SET estado="cancelada" WHERE id_entrada = 1;
--- Ejercicio 4: cambiar precio entrada vip,
--- En modo seguro de Workbench SQL no admite cambiar si no es el id: Error Code: 1175. 
--- You are using safe update mode and you tried to update a table without a WHERE that uses a KEY column. 
--- To disable safe mode, toggle the option in Preferences -> SQL Editor and reconnect.
+-- Ejercicio 4: cambiar precio entrada vip
 
--- UPDATE tipos_entrada SET precio=160 WHERE nombre_tipo="VIP" ;
-UPDATE tipos_entrada SET precio=160 WHERE id_tipo=2 ; 
+UPDATE tipos_entrada SET precio=160 WHERE nombre_tipo="VIP" ;
+-- UPDATE tipos_entrada SET precio=160 WHERE id_tipo=2 ; 
 
 -- Ejercicio 5: actualizar el aforo del evento : rock universitario , nuevo aforo a 600
+UPDATE eventos SET aforo_total=600 WHERE titulo='Concierto Rock Universitario';
 
 -- Ejercicio 6: cambiar el metodo de pago tarjeta por efectivo donde el identificador es 9
 -- Ejercicio 7: actualizar la asistencia del evento 2 
