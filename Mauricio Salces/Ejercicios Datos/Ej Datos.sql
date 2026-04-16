9a8b08806e6b66ebbe6e820d1d123bf4bb602cb7
Rivanrete <rivanrete.eternavir@gmail.com>
2026-04-16 11:30:39 -0400

    ej 4

--- Mauricio Salces/Ejercicios Datos/Ej Datos.sql
+++ Mauricio Salces/Ejercicios Datos/Ej Datos.sql
@@ -7,6 +7,20 @@ INSERT INTO usuarios (usuario, contrasena, correo, nombre_completo, id_rol) VALU
 ('ardi','koneko','ardi@unifest.edu','Alvin Fernando Tarifa Robles',4),
 ('rivan','dxd','rivan@unifest.edu','Mauricio Ivan Salces Sanchez',5);
 
+-- insert del ej 4
+INSERT INTO entradas (id_evento, id_tipo, id_comprador, codigo_qr, entrega, estado)
+VALUES
+(1, 1, 7, 'QR-006-KORE', 'fisica', 'vendida'),
+(2, 2, 7, 'QR-007-KORE', 'digital', 'reservada'),
+(2, 2, 8, 'QR-008-ARDI', 'digital', 'cancelada'),
+(2, 2, 9, 'QR-009-RIVAN', 'digital', 'reservada');
+INSERT INTO pagos (id_entrada, id_pagador, monto, metodo_pago)
+VALUES
+(6, 4, 40.00, 'efectivo'),
+(7, 5, 15.00, 'online'),
+(8, 4, 40.00, 'efectivo'),
+(9, 5, 100.00, 'tarjeta');
+
 Update usuarios
 set correo = 'rivanrete@unifest.edu'
 where id_usuario= 9;
@@ -25,3 +39,9 @@ where id_tipo= 2;
 Update eventos
 set aforo_total = 600
 where id_evento= 1;
+
+-- Ejercicio 4
+Update pagos
+set metodo_pago = 'efectivo'
+where id_pago= 9;
+
