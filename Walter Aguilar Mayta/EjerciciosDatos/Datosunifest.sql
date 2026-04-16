49081ad5998104b6af3472875c061f3d20071e40
walter-202 <wfernando.aguilarm@gmail.com>
2026-04-16 11:15:50 -0400

    Ejercicio 1

---
+++ Walter Aguilar Mayta/EjerciciosDatos/Datosunifest.sql
@@ -0,0 +1,15 @@
+
+-- Ejercicio 1: Insertar 2 roles
+INSERT INTO roles (nombre_rol, descripcion) VALUES
+('supervisor','Supervisor en el evento '),
+('guardia','Guardia en el evento');
+
+-- Ejercicio 2: Insertar 3 usuarios
+
+-- Ejercicio 3: Cancelar una entrada
+-- Ejercicio 4: cambiar precio entrada vip,
+ 
+-- Ejercicio 5: actualizar el aforo del evento : rock universitario , nuevo aforo a 600
+
+-- Ejercicio 6: cambiar el metodo de pago tarjeta por efectivo donde el identificador es 9
+-- Ejercicio 7: actualizar la asistencia del evento 2 
