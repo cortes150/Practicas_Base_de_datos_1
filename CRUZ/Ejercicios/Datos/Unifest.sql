534ed9fec3f3a98b50bbed00ccb01e99259c7ada
SongGin <songgin091@gmail.com>
2026-04-16 11:21:21 -0400

    Ejercicio 1

---
+++ CRUZ/Ejercicios/Datos/Unifest.sql
@@ -0,0 +1,16 @@
+--Ejercicio 1
+--cambiar el estado de una entrada a cancelado
+update entradas
+set estado 'cancelada'
+where id_entrada = 6
+--Ejercicio 2
+--cambiar el precio de la entrada vip
+
+--Ejercicio 3
+--actualizar el aforo del evento rock universitario, que el nuevo aforo sea 600
+
+--Ejercicio 4
+--cambiar el metodo de pago tarjeta por efectivo donde el identificador es 9
+
+--Ejercicio 5
+--actualizar la asistencia del evento 2 con su identificador 2 
