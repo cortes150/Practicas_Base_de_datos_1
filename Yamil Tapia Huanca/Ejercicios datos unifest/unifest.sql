ae9f313a5f18b07043201ae5885d1765b2281ef1
TapiaYami-coder <tapiahuancayami@gmail.com>
2026-04-16 11:42:07 -0400

    E2

--- Yamil Tapia Huanca/Ejercicios datos unifest/unifest.sql
+++ Yamil Tapia Huanca/Ejercicios datos unifest/unifest.sql
@@ -6,6 +6,9 @@ set estado = 'cancelada'
 where id_entrada = 4;
 
 -- E2: cambiar el precio de la entrada vip
+update tipos_entrada
+set precio= 120.00
+where id_tipo = 4;
 -- E3: actualizar el aforo del evento rock universitario a 600
 -- E4: Cambiar el metodo de pago targeta ´por efectivo donde el id es 9
 -- E5: actualizar la asistencia del evento 2  id 2 
