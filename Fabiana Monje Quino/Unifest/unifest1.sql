ade9aacac6f691d1310fc88b3813300742d324b1
Fabiana765 <fabianacarelizm@gmail.com>
2026-04-16 11:53:41 -0400

    Agregando actualizando al ejercicio5

--- Fabiana Monje Quino/Unifest/unifest1.sql
+++ Fabiana Monje Quino/Unifest/unifest1.sql
@@ -9,8 +9,16 @@ update entradas set estado ='cancelada' where id_tipo = 2
 update eventos set aforo_total = 600 where id_evento = 1
 --actualizar el aforo del evento rock que sea 600
 --EJERCICIO 4--
+INSERT INTO pagos (id_pago, id_entrada, id_pagador, monto, metodo_pago) VALUES
+(6, 1, 3, 20.00, 'efectivo'),
+(7, 2, 3, 15.50, 'online'),
+(8, 3, 4, 10.00, 'tarjeta');
 INSERT INTO pagos (id_pago, id_entrada, id_pagador, monto, metodo_pago) 
 VALUES (9, 1, 3, 50.00, 'tarjeta');
 update pagos set metodo_pago = 'efectivo' where id_pago = 9
 --cambiar el metodo de pago tarjeta por efectivo donde el identificador es 9
+--EJERCICIO 5--
+UPDATE asistencia 
+SET asistio = 1 
+WHERE id_evento = 2;
 --actualizar la asistencia del evento 2
