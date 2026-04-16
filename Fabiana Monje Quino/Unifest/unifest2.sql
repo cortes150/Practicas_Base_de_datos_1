e039aed499fb0e5ca5dcdb48caf201e6b20bda53
Fabiana765 <fabianacarelizm@gmail.com>
2026-04-16 11:30:09 -0400

    Agregando actualizando ejercicio2

--- Fabiana Monje Quino/Unifest/unifest2.sql
+++ Fabiana Monje Quino/Unifest/unifest2.sql
@@ -1,10 +1,11 @@
 --EJERCICIO 1--
 --cambiar el precio de la entrada  vip
-select * from entradas
+select * from eventos
 update tipos_entrada set precio ='300' where id_tipo = 2
 --EJERCICIO 2--
 --cambiar el estado de una entrada a cancelada
 update entradas set estado ='cancelada' where id_tipo = 2
+
 --actualizar el aforo del evento rock que sea 600
 --cambiar el metodo de pago tarjeta por efectivo donde el identificador es 9
 --actualizar la asistencia del evento 2
