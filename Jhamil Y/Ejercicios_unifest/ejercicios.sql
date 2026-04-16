058b142419bddb797d5717f75954887718e81940
Trivilin76 <jhamil12mamani@gmail.com>
2026-04-16 11:51:59 -0400

    Update ejercicios.sql

--- Jhamil Y/Ejercicios_unifest/ejercicios.sql
+++ Jhamil Y/Ejercicios_unifest/ejercicios.sql
@@ -2,3 +2,13 @@
 update entradas 
 set estado = 'cancelar'
 where id_entrada = 3;
+
+ -- ejercicio 2 --
+update tipos_entrada
+set precio = 100.00
+where id_tipo = 3;
+ 
+ -- ejercicio 3 --
+update eventos 
+set aforo_total = 600
+where id_evento = 1;
