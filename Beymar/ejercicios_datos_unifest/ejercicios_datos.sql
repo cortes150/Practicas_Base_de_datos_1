11b07781bb036b8d9b95996ab3e87bf685ec64af
NombreDelCompa <ye1c0vh@gmail.com>
2026-04-16 11:27:41 -0400

    ejercicio 1

---
+++ Beymar/ejercicios_datos_unifest/ejercicios_datos.sql
@@ -0,0 +1,8 @@
+##EJERCICIOS
+## 1. cambiar una entrada su estado a cancelada
+update entradas set estado = 'cancelada' where id_entrada = 4;
+## 2. cambiar el precio de la entrada VIP
+
+## 3. actualizar el aforo del evento rock universitario que sea 600
+## 4. cambiar el metodo de pago tarjeta por ejectivo donde el indentificador es 9
+## 5. actualizar la asistencia del evento 2 
