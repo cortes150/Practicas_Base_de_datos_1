50d8982452031f19d79ae6939476c99c866a5c69
wilfredocusiquispe-cell <wilfredocusiquispe@gmail.com>
2026-04-24 11:26:22 -0400

    Update consultas_examen.sql

--- examen_final/consultas_examen.sql
+++ examen_final/consultas_examen.sql
@@ -22,7 +22,32 @@ group by p.metodo;
 -- 4.-Muestra los eventos que tienen al menos una entrada cancelada y cuántas fueron
 
 -- 5.- Muestra los usuarios que compraron más de una entrada
-
+select u.nombre as Usuario , count(en.id_entrada) as entradas_adquiridas
+from usuario u
+join compra c on c.id_usuario = u.id_usuario
+join entrada en on c.id_entrada = en.id_entrada
+group by u.id_usuario
+having entradas_adquiridas > 1;
 -- 6.- Encuentra el evento con la entrada más cara del sistema.
 
 -- 7.- Muestra los usuarios que asistieron al menos a un evento.
+select u.nombre as Usuario , a.asistio
+from usuario u 
+join asistencia a on u.id_usuario = a.id_usuario
+where a.asistio = 1
+order by u.id_usuario;
+
+-- 8.- Muestra el promedio de precios de las entradas por cada evento.
+
+-- 9.- Muestra los usuarios que no asistieron a ningún evento. Pista: Puedes usar una subconsulta con NOT IN. Pista: Puedes usar una subconsulta con NOT IN.
+
+
+-- 10.- Muestra cuánto dinero ha pagado en total cada usuario, ordenado de mayor a menor.
+select u.nombre as Usuario , sum(p.monto) as Total_gastado
+from usuario u 
+join compra c on c.id_usuario = u.id_usuario
+join pago p on c.id_compra = p.id_compra
+group by u.id_usuario
+order by Total_gastado desc;
+
+select * from usuario 
